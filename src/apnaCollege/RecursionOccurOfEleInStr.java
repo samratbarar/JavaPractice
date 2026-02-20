4a69d45236bcb201a7a010a933cfbcc30503a99e
samratbarar <samratbarar7707@gmail.com>
2026-02-20 21:18:12 +0530

    modification in occurance question

--- src/apnaCollege/RecursionOccurOfEleInStr.java
+++ src/apnaCollege/RecursionOccurOfEleInStr.java
@@ -2,9 +2,15 @@ package apnaCollege;
 
 import java.util.*;
 
+/*
+find the index of first and last of any character in string using recursion
+ */
+
 public class RecursionOccurOfEleInStr {
-    public static void Occurance(int first, int sec, int l, String str, String key) {
-        if (l == str.length()-1) {
+    public static int first = -1;
+    public static int sec = -1;
+    public static void Occurance(int l, String str, String key) {
+        if (l == str.length()) {
             System.out.println("The first and last occurance indexes are " + first + " and " + sec);
             return;
         }
@@ -14,7 +20,7 @@ public class RecursionOccurOfEleInStr {
         if (str.charAt(l) == key.charAt(0)) {
             sec = l;
         }
-        Occurance(first, sec, l+1, str, key);
+        Occurance(l+1, str, key);
     }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -23,6 +29,6 @@ public class RecursionOccurOfEleInStr {
         String str = sc.nextLine();
         String key = sc.next();
 
-        Occurance(-1, -1, 0, str, key);
+        Occurance(0, str, key);
     }
 }
