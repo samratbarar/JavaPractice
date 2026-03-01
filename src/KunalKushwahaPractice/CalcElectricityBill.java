caff4982fcc21f0c541b6b65cfcc0f2a5754e255
samratbarar <samratbarar7707@gmail.com>
2026-03-02 00:36:15 +0530

    New question of electricity bill calculation is added

--- src/KunalKushwahaPractice/CalcElectricityBill.java
+++ src/KunalKushwahaPractice/CalcElectricityBill.java
@@ -6,5 +6,30 @@ public class CalcElectricityBill {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
+        System.out.println("Enter your previous and current meter readings : ");
+        float prevRead = sc.nextFloat(), currRead = sc.nextFloat();
+
+        System.out.println("Enter fixed charges on electricity of your area : ");
+        int fixCharge = sc.nextInt();
+
+        System.out.println("Enter tax(in %) on electricity in your area : ");
+        float tax = sc.nextFloat();
+
+        float units = currRead - prevRead;
+        if (units < 0) {
+            System.out.println("Readings are invalid");
+            return;
+        }
+
+        float bill = 0;
+        if (units <= 100) {
+            bill = units*3;
+        } else if ((currRead - prevRead) <= 200) {
+            bill = 100*3 + (units-100)*5;
+        } else {
+            bill = 100*3 + 100*5 + (units-200)*7;
+        }
+
+        System.out.println("Your bill is " + (bill + fixCharge + bill*tax/100));
     }
 }
