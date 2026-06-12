f0aa4c1f3cd7e49df639d31a91011a9b5bec5c96
samratbarar <samratbarar7707@gmail.com>
2026-06-12 23:52:28 +0530

    Stack Question pushing at bottom

---
+++ src/apnaCollege/CollectionFrameWorks/StackDataStructure/pushAtBottom.java
@@ -0,0 +1,29 @@
+package apnaCollege.CollectionFrameWorks.StackDataStructure;
+
+import java.util.*;
+
+public class pushAtBottom {
+    public static void pushingAtBottom(int data, Stack<Integer> s) {
+        if (s.isEmpty()) {
+            s.push(data);
+            return;
+        }
+
+        int top = s.pop();
+        pushingAtBottom(data, s);
+        s.push(top);
+    }
+    public static void main(String[] args) {
+        Stack<Integer> s = new Stack<>();
+
+        s.push(1);
+        s.push(2);
+        s.push(3);
+
+        pushingAtBottom(4,s);
+
+        while (!s.isEmpty()) {
+            System.out.println(s.pop());
+        }
+    }
+}
